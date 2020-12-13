0b818b8374f66fd0ac79470aa2f15593522212e1
Tholke <oehlke.thomas@gmail.com>
2020-12-13 19:23:05 +0100

    finished exercise06

--- src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestClassifier.java
+++ src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestClassifier.java
@@ -1,24 +1,50 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
 public class TestClassifier {
-
+	private Classifier classifier = new Classifier();
+	private Instances data;
+	
 	@BeforeEach
-	public void setup() {
-
+	public void setup() throws Exception{
+		//Mocking it would be better, I guess. 
+		DataSource loader = new DataSource("src/main/resources/germancredit/train.arff");
+		data = loader.getDataSet();
 	}
 
 	@Test
 	public void testEntropy() {
-		assertTrue(true); // <-- this needs to be changed
+		assertThrows(IllegalArgumentException.class, () -> {
+			classifier.entropy(null);
+		});
+		assertTrue(classifier.entropy(data) >= 0);
+		
+		//We have to know the outcome of the method for this test. Better with mocking?
+		assertEquals(1, classifier.entropy(data));
 	}
 
 	@Test
 	public void testInformationGain() {
-		assertTrue(true); // <-- this needs to be changed
+		assertThrows(IllegalArgumentException.class, () -> {
+			classifier.informationGain(null, 0);
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			classifier.informationGain(data, -1);
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			classifier.informationGain(data, data.numAttributes() + 1);
+		});
+
+		//We have to know the outcome of the method for this test. Better with mocking?
+		assertEquals(1, classifier.informationGain(data, 0));	
 	}
 }
