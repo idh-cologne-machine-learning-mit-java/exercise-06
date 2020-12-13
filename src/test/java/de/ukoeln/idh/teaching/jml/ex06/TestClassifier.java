e915c1abbd10829c735bdafbe6b3d45f820fce0e
stereolith <mail@lukasmoench.de>
2020-12-13 13:51:04 +0100

    add tests

--- src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestClassifier.java
+++ src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestClassifier.java
@@ -1,24 +1,69 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.io.File;
+import java.io.IOException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import de.ukoeln.idh.teaching.jml.ex06.Classifier;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+
 public class TestClassifier {
 
+	private Classifier classifier = new Classifier();
+	private Instances instances;
+	private Tree output;
+	
 	@BeforeEach
-	public void setup() {
-
+	public void setup() throws IOException {
+		ArffLoader loader = new ArffLoader();
+		loader.setFile(new File("src/main/resources/amazon/train.arff.gz"));		
+		instances = loader.getDataSet();
+		System.out.println(instances.numAttributes());
+		
+		output = classifier.train(instances);
+	}
+	
+	@Test
+	public void testTrain() {
+		// test null param input
+		assertThrows(Exception.class, () -> classifier.train(null));
+	}
+	
+	@Test
+	public void testTrainOutput() {
+		// test output
+		assertFalse(output.isLeaf());
 	}
 
 	@Test
 	public void testEntropy() {
-		assertTrue(true); // <-- this needs to be changed
+		// test empty input 
+		assertThrows(Exception.class, () -> classifier.entropy(null));
+		
+		// test entropy value
+		double e = classifier.entropy(instances);
+		assertTrue(0.0 <= e && e <= 1.0);
 	}
 
 	@Test
 	public void testInformationGain() {
-		assertTrue(true); // <-- this needs to be changed
+		// test empty instances parameter
+		assertThrows(Exception.class, () -> classifier.informationGain(null, 0));
+		
+		// test bad attributeIndex value
+		assertThrows(Exception.class, () -> classifier.informationGain(instances, -1));
+		assertThrows(Exception.class, () -> classifier.informationGain(instances, instances.numAttributes() + 1));
+		
+		// test output
+		double i = classifier.informationGain(instances, 0);
+		assertTrue(0.0 <= i && i <= 1.0);
+		
 	}
 }
--- src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestTree.java
+++ src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestTree.java
@@ -1,19 +1,47 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import weka.core.DenseInstance;
+import de.ukoeln.idh.teaching.jml.ex06.Tree;
+
 public class TestTree {
+	
+	private Tree tree = new Tree();
+	
 	@BeforeEach
 	public void setup() {
-
+	
 	}
 
 	@Test
 	public void testIsLeaf() {
-		assertTrue(true); // <-- this needs to be changed
+		tree.children = null;
+		assertTrue(tree.isLeaf(), "isLeaf() returns false for leaf node");
+		Tree child = new Tree();
+		tree.children = new Tree[] {child};
+		assertFalse(tree.isLeaf(), "isLeaf() isLeaf() returns true for non-leaf node");
+	}
+	
+	
+	@Test
+	public void testPredict() {
+		// test null param input
+		assertThrows(Exception.class, () -> tree.predict(null));
+		
+		// test instance that has no attribute matching attributeIndex
+		tree.attributeIndex = 2;
+		assertThrows(Exception.class, () -> tree.predict(new DenseInstance(1)));
+		
+		// test prediciotn is in range 0-1 (incl.)
+		tree.attributeIndex = 0;
+		double prediction = tree.predict(new DenseInstance(1));
+		assertTrue(0.0 <= prediction && prediction <= 1.0);
 	}
 
 }
