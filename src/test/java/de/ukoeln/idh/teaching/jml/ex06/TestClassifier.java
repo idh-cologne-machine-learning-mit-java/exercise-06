c5df290aa5f9befede847d3b4327afbb5ce99c30
eliasuk <efaqirza@smail.uni-koeln.de>
2020-12-15 00:07:17 +0100

    Done with the exercise

--- src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestClassifier.java
+++ src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestClassifier.java
@@ -1,11 +1,21 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+import weka.core.Instances;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestClassifier {
+
+	private Classifier klassifikator = new Classifier();
+	private Instances data;
 
 	@BeforeEach
 	public void setup() {
@@ -14,11 +24,42 @@ public class TestClassifier {
 
 	@Test
 	public void testEntropy() {
-		assertTrue(true); // <-- this needs to be changed
+		
+		assertTrue(klassifikator.entropy(data) >= 0);
+	}
+
+	@Test
+	public void testEntropyInput() {
+		assertThrows(NullPointerException.class, () -> {
+			klassifikator.entropy(null);
+		});
 	}
 
 	@Test
 	public void testInformationGain() {
-		assertTrue(true); // <-- this needs to be changed
+		
+		assertTrue(klassifikator.informationGain(data, 0/*data.classIndex()*/) >= 0);
+	}
+
+	@Test
+	public void testInformationGainInputNull() {
+		assertThrows(NullPointerException.class, () -> {
+			klassifikator.informationGain(null, 0);
+		});
+	}
+
+	@Test
+	public void testInformationGainInputIllegal() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			klassifikator.informationGain(data, -1);
+		});
 	}
 }
+	
+	
+	
+	
+	
+	
+	
+
--- src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestTree.java
+++ src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestTree.java
@@ -1,19 +1,35 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestTree {
-	@BeforeEach
-	public void setup() {
 
-	}
+	private Tree baum_eins;
+	private Tree baum_zwei;
 
-	@Test
-	public void testIsLeaf() {
-		assertTrue(true); // <-- this needs to be changed
-	}
+@BeforeEach
+public void setup() {
 
+	baum_eins = mock(Tree.class);
+
+	baum_zwei = mock(Tree.class);
+	baum_zwei.children = mock(Tree[].class);
 }
+
+@Test
+public void testIsLeaf() {
+
+	assertTrue(baum_eins.isLeaf());
+}
+
+@Test
+public void testIsNoLeaf() {
+	assertFalse(baum_zwei.isLeaf());
+}
+}
+
