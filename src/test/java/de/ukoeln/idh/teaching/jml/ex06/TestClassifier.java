7ddb57164c9ec85459c937f9091ea2eafa6b62c2
laXta <mkrosta@smail.uni-koeln.de>
2020-12-14 17:26:21 +0100

    done

--- src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestClassifier.java
+++ src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestClassifier.java
@@ -1,24 +1,57 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+
+
 public class TestClassifier {
 
-	@BeforeEach
-	public void setup() {
+//
+//	@Mock
+//	Instances testInstances;
 
+	private Classifier classifier;
+	private Instances testData;
+
+
+	private Instances loadData(String path) throws IOException {
+		FileReader file = new FileReader(path);
+		BufferedReader data = new BufferedReader(file);
+		return new Instances(data);
 	}
+
+	@BeforeEach
+	public void setup() throws IOException {
+		testData = loadData("src/resources/test.arff"); // not in git, so one needs to specify an own dataset
+		classifier = new Classifier();
+	}
+
 
 	@Test
 	public void testEntropy() {
-		assertTrue(true); // <-- this needs to be changed
+		double entropy = classifier.entropy(testData);
+		assertTrue(0 <= entropy && entropy <= 1);
 	}
+
 
 	@Test
 	public void testInformationGain() {
-		assertTrue(true); // <-- this needs to be changed
+
+		double informationGain = classifier.informationGain(testData, testData.numAttributes() - 1);
+
+		assertTrue(0 <= informationGain && informationGain <= 1);
 	}
 }
--- src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestTree.java
+++ src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestTree.java
@@ -5,15 +5,24 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class TestTree {
+
+	Tree testTree;
+
 	@BeforeEach
 	public void setup() {
-
+		testTree = new Tree();
 	}
 
 	@Test
 	public void testIsLeaf() {
-		assertTrue(true); // <-- this needs to be changed
+
+		assertTrue(
+				testTree.children != null && !testTree.isLeaf() || // tree has no children then its not a tree (explicit)
+				testTree.children == null && testTree.isLeaf() // // tree has children then its not a leave here  (explicit)
+		);
 	}
 
 }
