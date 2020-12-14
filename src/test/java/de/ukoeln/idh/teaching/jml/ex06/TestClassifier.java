a1a07344bf1e33b0ef80a8ae3d5a80b77fa9fb73
krause-s <skrause.koeln@gmail.com>
2020-12-14 18:04:51 +0100

    implemented unit tests

--- src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestClassifier.java
+++ src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestClassifier.java
@@ -1,24 +1,53 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+import weka.core.Instances;
+
 public class TestClassifier {
+
+		private Classifier classifier = new Classifier();
+		private Instances data;
 
 	@BeforeEach
 	public void setup() {
-
+		data = mock(Instances.class);
 	}
 
 	@Test
 	public void testEntropy() {
-		assertTrue(true); // <-- this needs to be changed
+		assertTrue(classifier.entropy(data) >= 0);
+	}
+
+	@Test
+	public void testEntropyInput() {
+		assertThrows(NullPointerException.class, () -> {
+			classifier.entropy(null);
+		});
 	}
 
 	@Test
 	public void testInformationGain() {
-		assertTrue(true); // <-- this needs to be changed
+		assertTrue(classifier.informationGain(data, 0/*data.classIndex()*/) >= 0);
 	}
+	
+	@Test
+	public void testInformationGainInputNull() {
+		assertThrows(NullPointerException.class, () -> {
+			classifier.informationGain(null, 0);
+		});
+	}
+	
+	@Test
+	public void testInformationGainInputIllegal() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			classifier.informationGain(data, -1);
+		});
+	}
+	
 }
--- src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestTree.java
+++ src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestTree.java
@@ -1,19 +1,32 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestTree {
+	
+		private Tree tree1, tree2;
+	
 	@BeforeEach
 	public void setup() {
-
+		tree1 = mock(Tree.class);
+		
+		tree2 = mock(Tree.class);
+		tree2.children = mock(Tree[].class);
 	}
 
 	@Test
 	public void testIsLeaf() {
-		assertTrue(true); // <-- this needs to be changed
+		assertTrue(tree1.isLeaf());
+	}
+	
+	@Test
+	public void testIsNoLeaf() {
+		assertFalse(tree2.isLeaf());
 	}
 
 }
