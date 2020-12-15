3c43d0acddbaa0ccc2de199ec4919a01e217f1a8
MoritzMeid <ameid@smail.uni-koeln.de>
2020-12-15 12:47:04 +0100

    YML

--- src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestTree.java
+++ src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestTree.java
@@ -1,5 +1,6 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -17,6 +18,12 @@ public class TestTree {
 	@Test
 	public void testIsLeaf() {
 		assertTrue(testTree.isLeaf()); // <-- this needs to be changed
+		
+		testTree.children = new Tree[] {};
+		assertTrue(testTree.isLeaf());
+		
+		testTree.children = new Tree[] {new Tree(), new Tree()};
+		assertFalse(testTree.isLeaf());
 	}
 
 }
