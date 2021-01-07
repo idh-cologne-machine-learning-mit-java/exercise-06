193cac8effd37b33f8bae95c7c40aa80b9cc8c5e
Cosima Granderath-Miegel <cgrander@smail.uni-koeln.de>
2021-01-07 09:06:36 +0100

    Done

--- src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestClassifier.java
+++ src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestClassifier.java
@@ -1,24 +1,56 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
 public class TestClassifier {
 
-	@BeforeEach
-	public void setup() {
+	Classifier classifier = new Classifier();
+	Instances data;
+	Tree results;
 
+
+	@BeforeEach
+	public void setup() throws Exception{
+		DataSource source = new DataSource("src/resources/germancredit/train.arff");
+		data = source.getDataSet();
+		if(data.classIndex() == -1){
+			data.setClassIndex(data.numAttributes() - 1);
+		}
+		System.out.println(data.numAttributes());
+	}
+
+	@Test
+	public void testTrain(){
+		results = classifier.train(data);
+		assertThrows(IllegalArgumentException.class,()->{classifier.train(null);});
 	}
 
 	@Test
 	public void testEntropy() {
-		assertTrue(true); // <-- this needs to be changed
+		//illegal input
+		assertThrows(IllegalArgumentException.class, ()->{classifier.entropy(null);});
+
+		double H = classifier.entropy(data);
+		//this is not really correct, as entropy could be greater than 1
+		//but deciding on another limiting case is difficult without actual values
+		assertTrue(0.0 <= H && H <= 1.0);
+
 	}
 
 	@Test
 	public void testInformationGain() {
-		assertTrue(true); // <-- this needs to be changed
+		assertThrows(IllegalArgumentException.class, () -> {classifier.informationGain(null, Integer.MIN_VALUE);});
+		assertThrows(IllegalArgumentException.class, () -> {classifier.informationGain(data, -1);});
+		assertThrows(IllegalArgumentException.class, ()->{classifier.informationGain(data, data.numAttributes()+1);});
+
+		double ig = classifier.informationGain(data, 0);
+		assertTrue(0.0 <= ig && ig <= 1.0);
 	}
 }
--- src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestTree.java
+++ src/test/java/de/ukoeln/idh/teaching/jml/ex06/TestTree.java
@@ -1,19 +1,33 @@
 package de.ukoeln.idh.teaching.jml.ex06;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestTree {
+	Tree tree = new Tree();
 	@BeforeEach
 	public void setup() {
 
 	}
 
 	@Test
+	public void testPredict(){
+		assertThrows(IllegalArgumentException.class,()->{tree.predict(null);});
+	}
+	@Test
 	public void testIsLeaf() {
-		assertTrue(true); // <-- this needs to be changed
+		//assertTrue(true); // <-- this needs to be changed
+		assertTrue(tree.isLeaf());
+
+		tree.children = null;
+		assertTrue(tree.isLeaf());
+
+		tree.children = new Tree[] {new Tree(), new Tree()};
+		assertFalse(tree.isLeaf());
 	}
 
 }
